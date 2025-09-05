7e9f6913972178468e316e10f03532eaa959a300
Luana Lima <luana.lima@sou.inteli.edu.br>
2025-09-05 12:34:50 -0300

    feat: limpar os dados filtrando os dias em que as visualizações de página estavam entre os 2,5% superiores ou os 2,5% inferiores do conjunto de dados

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -8,7 +8,7 @@ register_matplotlib_converters()
 df = pd.read_csv("fcc-forum-pageviews.csv", parse_dates=["date"], index_col="date")
 
 # Clean data
-df = None
+df = df[(df["value"] >= df["value"].quantile(0.025)) & (df["value"] <= df["value"].quantile(0.975))]
 
 
 def draw_line_plot():
