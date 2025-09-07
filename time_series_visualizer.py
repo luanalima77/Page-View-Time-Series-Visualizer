4f8b1ec7fc1ee96e477a7c847a15685a6d7fcd9c
Luana Lima <luana.lima@sou.inteli.edu.br>
2025-09-07 08:52:31 -0300

    feat: adicionar função draw_bar_plot

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -25,12 +25,16 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    df_bar = df.copy()
+    df_bar["year"] = df_bar.index.year
+    df_bar["month"] = df_bar.index.month
+    df_grouped = df_bar.groupby(["year", "month"])["value"].mean().unstack()
 
     # Draw bar plot
-
-
-
+    fig = df_grouped.plot(kind="bar", figsize=(10, 8)).figure
+    plt.xlabel("Years")
+    plt.ylabel("Average Page Views")
+    plt.legend(title="Months", labels=["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"])
 
 
     # Save image and return fig (don't change this part)
